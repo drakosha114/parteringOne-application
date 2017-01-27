ebb6102ebdc5e479d69207c5b2df7a3169ef5a2e
askrypai <drakosha114@gmail.com>
2017-01-27 10:49:29 +0200

    redisign

---
+++ config.js
@@ -0,0 +1,16 @@
+var __CONFIG__ = {
+    apiKey: '4qum_bZaHz3o_5o_Mgeyb-VmOHtw7IzJ',
+    dbName: 'partnersone',
+    collections: {
+        users: 'users',
+        companies: 'companies',
+        events: 'events',
+        categories: 'categories',
+        myEvents: 'myEvents'
+    },
+    apiUrl: 'https://api.mlab.com/api/1/databases'
+};
+
+var __MATHERIAL_THEME_CONFIG_ = {
+
+}
